53a9371d1acd40005a1492a223256690cdda149e
Joshua Davey <josh@joshuadavey.com>
2013-03-05 19:28:27 -0600

    Cleanup

--- main.m
+++ main.m
@@ -6,6 +6,7 @@ int main (int argc, const char * argv[]) {
 
     CGMutablePathRef path;
 
+    // Set up a fullscreen, transparent window/view
     NSWindow *window = [[NSWindow alloc] initWithContentRect: NSScreen.mainScreen.frame
                                                    styleMask: NSBorderlessWindowMask
                                                      backing: NSBackingStoreBuffered
@@ -14,46 +15,46 @@ int main (int argc, const char * argv[]) {
     [window setOpaque: NO];
     [window setLevel: NSFloatingWindowLevel];
 
-    NSString *imagePath = [NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @".unicorn.png"];
-
-    NSImage *image = [[NSImage alloc] initWithContentsOfFile: imagePath];
-    /* CGImageSourceRef source; */
-    /* source = CGImageSourceCreateWithData((__bridge CFDataRef)[image TIFFRepresentation], NULL); */
-    /* CGImageRef cgimage = CGImageSourceCreateImageAtIndex(source, 0, NULL); */
-
     NSView *view = [[NSView alloc] initWithFrame:NSScreen.mainScreen.frame];
     [window setContentView: view];
 
+    // Choose image to display
+    NSString *imagePath = [NSString stringWithFormat:@"%@/%@", NSHomeDirectory(), @".unicorn.png"];
 
+    // Get image dimensions
+    NSImage *image = [[NSImage alloc] initWithContentsOfFile: imagePath];
+    CGSize imageSize = image.size;
+    [image dealloc];
+
+
+    // Load image as CGImage
     CGDataProviderRef source = CGDataProviderCreateWithFilename([imagePath UTF8String]);
-
     CGImageRef cgimage = CGImageCreateWithPNGDataProvider(source, NULL, true, 0);
 
-    {   // Create a path to animate a layer on. We will also draw the path.
-        path = CGPathCreateMutable();
-        CGPoint origin = CGPointMake(-image.size.width, -image.size.height);
-        CGPoint destination = CGPointMake(view.frame.size.width + image.size.width, origin.y);
-        CGFloat midpoint = (destination.x + origin.x) / 2.0;
-        CGFloat peak = image.size.height;
-        CGPathMoveToPoint(path, NULL, origin.x, origin.y);
+    // Create a path to animate a layer on. We will also draw the path.
+    path = CGPathCreateMutable();
+    CGPoint origin = CGPointMake(-imageSize.width, -imageSize.height);
+    CGPoint destination = CGPointMake(view.frame.size.width + imageSize.width, origin.y);
+    CGFloat midpoint = (destination.x + origin.x) / 2.0;
+    CGFloat peak = imageSize.height;
+    CGPathMoveToPoint(path, NULL, origin.x, origin.y);
 
-        CGPathAddCurveToPoint(path, NULL, midpoint, peak,
-                              midpoint, peak,
-                              destination.x, destination.y);
+    CGPathAddCurveToPoint(path, NULL, midpoint, peak,
+                            midpoint, peak,
+                            destination.x, destination.y);
 
-    }
-
-    // This is the layer that animates along the path.
+    // Create layer for image; This is the layer that animates
     CALayer *layer = [CALayer layer];
 
     [layer setContents: (id)(cgimage)];
-    [layer setBounds:CGRectMake(0.0, 0.0, image.size.width, image.size.height)];
-    [layer setPosition:CGPointMake(-image.size.width, -image.size.height)];
+    [layer setBounds:CGRectMake(0.0, 0.0, imageSize.width, imageSize.height)];
+    [layer setPosition:CGPointMake(-imageSize.width, -imageSize.height)];
 
     [view setWantsLayer: YES];
 
     [view.layer addSublayer: layer];
 
+    // Create the path animation, and add it oto the layer
     CAKeyframeAnimation *animation = [CAKeyframeAnimation animationWithKeyPath:@"position"];
     [animation setPath: path];
     [animation setDuration: 2.0];
@@ -63,6 +64,8 @@ int main (int argc, const char * argv[]) {
     [layer addAnimation:animation forKey:@"position"];
 
     [window makeKeyAndOrderFront: nil];
+
+    // Wait for animation to finish
     [[NSRunLoop currentRunLoop] runUntilDate:[NSDate dateWithTimeIntervalSinceNow: animation.duration]];
 
     return 0;
