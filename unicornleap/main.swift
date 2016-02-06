c55f4a4c0d3b9b18f58b8500b4e11a345d4db140
Jon Allured <jon.allured@gmail.com>
2016-02-05 18:45:01 -0600

    Get man page and usage message in sync

--- unicornleap/main.swift
+++ unicornleap/main.swift
@@ -3,11 +3,11 @@ import Cocoa
 func printUsage(exitCode: Int32) {
   print(
     "Usage: unicornleap [options]\n",
-    "  -h  --help           Display this usage information.\n",
+    "  -h  --help           Display usage information.\n",
     "  -s  --seconds n      Animate for n seconds. (default: 2.0)\n",
     "  -n  --number i       Display i unicorns. (default: 1)\n",
-    "  -u  --unicorn file   Filename to use for unicorn image.\n",
-    "  -k  --sparkle file   Filename to use for sparkle image.\n",
+    "  -u  --unicorn file   Filename for unicorn image.\n",
+    "  -k  --sparkle file   Filename for sparkle image.\n",
     "  -v  --verbose        Print verbose messages."
   )
 
